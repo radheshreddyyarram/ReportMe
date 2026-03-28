e6a5cdf6161bd537be9251a608f493e0872e1fee
radheshreddyyarram <yarramradheshreddy@gmail.com>
2026-03-28 17:12:00 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '') || '';
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
